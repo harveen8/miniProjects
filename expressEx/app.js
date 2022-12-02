67c9cc8cf1ed22c75cd419c93771cc53bae97d79
Harveen G <harveen096@revature.net>
2022-12-02 09:05:40 -0800

    added node express example

---
+++ expressEx/app.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const cors = require('cors');
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+let listOfMovies= ["Nightcrawler", "Barbarian", "Inception", "It Follows"]
+let comments= ["I love all these movies!", "I could watch It Follows everyday", "Not a fan of Nightcrawler"]
+app.get('/', (req, res) => {
+  res.send(listOfMovies)
+})
+app.post('/', (req, res) => {
+    listOfMovies.push(unCamelCase(req.body.movie));
+    res.send(listOfMovies);
+  })
+
+app.delete('/:movieName', (req,res)=>{
+    for(let i =0; i<listOfMovies.length;i++){
+        if(unCamelCase(req.params.movieName)==listOfMovies.at(i)){
+            listOfMovies.splice(i,1)
+        }
+    }
+    res.send(listOfMovies)
+})
+app.get('/comments', (req, res) => {
+    res.send(comments);
+  })
+app.post('/comments', (req, res) => {
+    comments.push(req.body.comments);
+    res.send(comments);
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+
+//------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function camelCase(s) {
+    let ar=s.toLowerCase().split(" ");
+    let ans="";
+    for(let i=0; i< ar.length; i++){
+        if(i!=0){
+           ans+= (ar[i].charAt(0)).toUpperCase()+ar[i].substr(1)
+        }else{
+            ans+=ar[i];
+        }
+    }
+    return ans;
+}
+function unCamelCase(ans){
+    let camelcasedString =camelCase(ans);
+    let reverse= "";
+    for(let j=0; j<camelcasedString.length; j++){
+        if(j==0){
+            reverse+=camelcasedString.charAt(j).toUpperCase();
+        }else if(camelcasedString.charAt(j)==camelcasedString.charAt(j).toUpperCase()){
+            reverse+=" "+camelcasedString.charAt(j);
+        }else{
+            reverse+=camelcasedString.charAt(j);
+        }
+    }
+    return reverse;
+}
---
+++ expressEx/movies.js
@@ -0,0 +1,90 @@
+
+let movieListDocument= document.getElementById("movieList");
+let commentListDocument= document.getElementById("commentList");
+
+let movieInput= document.getElementById("movieInput");
+let commentInput= document.getElementById("commentInput");
+
+let movieButton=document.getElementById("movieButton");
+movieButton.onclick=addMovie;
+let commentButton=document.getElementById("commentButton");
+commentButton.onclick=addComment;
+
+let movieMessage= document.getElementById("movieMessage");
+let commentMessage= document.getElementById("commentMessage");
+
+
+window.onload = async function() {
+   await loadMovies();
+    await loadComments();
+  };
+
+async function loadMovies(){
+    let response = await fetch("http://localhost:3000");
+    response = await response.json();
+    fillInMovies(response);
+}
+async function loadComments(){
+    let response2 = await fetch("http://localhost:3000/comments");
+    response2 = await response2.json();
+    fillInComments(response2);
+}
+function fillInMovies(response){
+    movieListDocument.innerHTML="";
+    let newList= document.createElement("ul");
+    for(let i=0; i< response.length; i++){
+        let newItem=document.createElement("li");
+        newItem.innerHTML=response[i];
+        newList.appendChild(newItem);
+    }
+    movieListDocument.appendChild(newList);
+}
+function fillInComments(response2){
+    commentListDocument.innerHTML="";
+    let newList= document.createElement("ul");
+    for(let i=0; i< response2.length; i++){
+        let newItem=document.createElement("li");
+        newItem.innerHTML=response2[i];
+        newList.appendChild(newItem);
+    }
+    commentListDocument.appendChild(newList);
+}
+async function addMovie(){
+    if(movieInput.value!=""){
+        movieMessage.innerHTML="";
+        let inputItem = {
+           movie: movieInput.value
+        }
+        let response = await fetch("http://localhost:3000", {
+            method:'POST',
+            mode:'cors',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body:JSON.stringify(inputItem)
+        });
+    }else{
+        movieMessage.innerHTML="Must input a movie name";
+    }
+    loadMovies();
+}
+async function addComment(){
+    if(commentInput.value!=""){
+        commentMessage.innerHTML="";
+        let inputItem = {
+           comments: commentInput.value
+        }
+        let response = await fetch("http://localhost:3000/comments", {
+            method:'POST',
+            mode:'cors',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body:JSON.stringify(inputItem)
+        });
+    }else{
+         commentMessage.innerHTML="Must input a comment";
+    }
+    loadComments();
+
+}
